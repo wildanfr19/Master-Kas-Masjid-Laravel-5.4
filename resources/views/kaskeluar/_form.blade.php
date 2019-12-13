9eed7171437d58c935d8b1bef3ed6bfc5710ef93
Wildan <fathurrohmanwildan136@gmail.com>
2019-12-13 13:48:58 +0700

    melakukan perubahan

--- resources/views/kaskeluar/_form.blade.php
+++ resources/views/kaskeluar/_form.blade.php
@@ -15,23 +15,14 @@
 	</div>
 	<br>
 	<div class="modal-footer">
-	{{-- 	@php $totaljumpem = 0; @endphp
-		@foreach($kasmasuk as $key => $item)
-		@php $totaljumpem += $item['jumlah']; @endphp
-		@endforeach
-		<p>Rp.{{ format_rupiah($totaljumpem) }}</p>
-		@php $totaljumpeng = 0; @endphp
-		@foreach($kaskeluar as $key => $item)
-		@php $totaljumpeng += $item['jumlah']; @endphp
-		@endforeach --}}
-	{{-- 	@if(count($kas[0]->jumlah > 0)) --}}
+
 		@php $totaljumpem = 0; @endphp
 		@foreach($kasmasuk as $key => $item)
 		     @php  
 		        $totaljumpem += $item['jumlah']; 
 		     @endphp
 		@endforeach
-		
+
 		@if($totaljumpem > '0')
 		<button type="submit"  class="btn btn-sm btn-primary "><i class="fas fa-save"></i> Simpan</button>
 		@else
